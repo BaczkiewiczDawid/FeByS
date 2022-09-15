e4f1564688580c9d8553bfcd383ce7581b5c0d56
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-09-15 21:11:10 +0200

    events header

--- src/styles/EventDetails.style.js
+++ src/styles/EventDetails.style.js
@@ -1,10 +1,71 @@
 import styled from "styled-components";
 
-export const Overlay = styled.div`
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -2;
 
+  img {
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -2;
+    width: 100vw;
+    height: 45vh;
+  }
+
+  @media screen and (min-width: 768px) {
+    img {
+      height: 50vh;
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    img {
+      height: 45vh;
+    }
+  }
 `;
 
+export const Overlay = styled.div`
+  width: 100vw;
+  height: 45vh;
+  position: absolute;
+  background-color: #2a2a2a;
+  opacity: 0.75;
+  z-index: -1;
 
-export const Image = styled.img`
+  @media screen and (min-width: 768px) {
+    height: 50vh;
+  }
 
+  @media screen and (min-width: 1440px) {
+    height: 45vh;
+  }
+`;
+
+export const Content = styled.section`
+  margin-top: 8rem;
+`;
+
+export const Title = styled.h1`
+  color: #fafafa;
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 1rem;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 3rem;
+  }
+
+  @media screen and (min-width: 1080px) {
+    font-size: 2rem;
+  }
+
+  @media screen and (min-width: 1440px) {
+    font-size: 2.5rem;
+    margin-top: 6rem;
+  }
 `;
--- src/templates/event-details.js
+++ src/templates/event-details.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
-import { Overlay, Image } from '../styles/EventDetails.style';
+import { Overlay, Wrapper, Content, Title } from "../styles/EventDetails.style";
 
 const EventDetails = ({ data }) => {
   const { html } = data.contentfulWydarzenia;
   const event = data.contentfulWydarzenia;
-console.log(event)
+  console.log(event);
 
   return (
     <Layout>
-      <Overlay>
-        <Image src={event.image.url} alt="dummyAlt" />
-      </Overlay>
-      <p>test</p>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <Wrapper>
+        <img src={event.image.url} alt="Kolarze w lesie" />
+        <Overlay />
+      </Wrapper>
+      <Title>{event.title}</Title>
+      <Content>
+        <p>test</p>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </Content>
     </Layout>
   );
 };

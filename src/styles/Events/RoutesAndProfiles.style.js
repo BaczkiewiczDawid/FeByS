083052865ca19be27e09c628c0fab1bba76eb958
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-09-22 11:45:35 +0200

    event routes and profiles desktop view

--- src/styles/Events/RoutesAndProfiles.style.js
+++ src/styles/Events/RoutesAndProfiles.style.js
@@ -5,6 +5,16 @@ export const Map = styled.div`
   height: 30vh;
   background-color: blue;
   margin: 2rem auto;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 4rem;
+    height: 35vh;
+  }
+
+  @media screen and (min-width: 1080px) {
+    margin-top: 4rem;
+    height: 45vh;
+  }
 `;
 
 export const InformationsWrapper = styled.section`
@@ -28,6 +38,35 @@ export const InformationsWrapper = styled.section`
   div {
     margin: 2rem 0;
   }
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+
+    div {
+        margin: none;
+        padding: 0 2.5rem;
+    }
+  }
+
+  @media screen and (min-width: 1080px) {
+    p {
+        font-size: 2.7rem;
+    }
+
+    h3 {
+        font-size: 1.2rem;
+    }
+
+    div {
+        padding: 1rem 4rem;
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    div {
+        padding: 2rem 7rem;
+    }
+  }
 `;
 
 export const ButtonsWrapper = styled.div`
@@ -35,6 +74,12 @@ export const ButtonsWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   margin: 1rem auto;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    margin-left: 2rem;
+    margin-top: 3rem;
+  }
 `;
 
 export const Button = styled.button`
@@ -50,5 +95,18 @@ export const Button = styled.button`
   &:hover {
     background-color: ${props => props.secondary ? 'none' : '#345FCE'};
     transition: .25s;
+  }
+
+  @media screen and (min-width: 768px) {
+    margin-top: 0;
+  }
+
+  @media screen and (min-width: 1080px) {
+    width: 30%;
+  }
+
+  @media screen and (min-width: 1440px) {
+    font-size: 1rem;
+    margin-top: 3rem;
   }
 `;

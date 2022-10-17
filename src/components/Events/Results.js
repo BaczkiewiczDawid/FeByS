0b7387cb4ebb80031df1a769b30d905cdd043c85
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-10-17 14:11:24 +0200

    contact page

---
+++ src/components/Events/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from './Wrapper';
+
+const Results = () => {
+  return (
+    <Wrapper>
+      <h2>Wyniki</h2>
+    </Wrapper>
+  )
+}
+
+export default Results
---
+++ src/pages/kontakt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Wrapper from '../components/About/Wrapper';
+
+const Contact = () => {
+  return (
+    <Layout secondary>
+      <Wrapper>
+        <h2>Kontakt</h2>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default Contact

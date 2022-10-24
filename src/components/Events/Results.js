ab4089a25edabcce5a26f17ab818ce69de3ca337
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-10-24 12:13:07 +0200

    render table from json data

--- src/components/Events/Results.js
+++ src/components/Events/Results.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import Wrapper from './Wrapper';
+import React from "react";
+import Wrapper from "./Wrapper";
+import data from "../../data/data.json";
+
+console.log(data.Arkusz1);
 
 const Results = () => {
   return (
     <Wrapper>
       <h2>Wyniki</h2>
+      <table>
+        <th>Open</th>
+        <th>Kat.</th>
+        <th>Kat</th>
+        <th>Imie i nazwisko</th>
+        <th>Drużyna</th>
+        <th>Czas</th>
+        <th>Kara</th>
+        {data.Arkusz1.map((el) => (
+          <tr>
+            <td>{el.open}</td>
+            <td>{el.katNum}</td>
+            <td>{el.kat}</td>
+            <td>{el.name}</td>
+            <td>{el.team}</td>
+            <td>{el.time}</td>
+            <td>{el.penalty}</td>
+          </tr>
+        ))}
+      </table>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Results
+export default Results;
